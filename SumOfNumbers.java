a8963a6a65445e75f23c0717c4f0b7f3f6e3c7ab
unknown <kaunghsetlwin349@gamil.com>
2024-08-11 17:15:00 +0630

    Add SumOfNumber

---
+++ SumOfNumbers.java
@@ -0,0 +1,13 @@
+
+public class SumOfNumbers {
+	public static void main(String args[]){
+		int result =0;
+	
+		for (int i = 1;i<=10;i++){
+		 result =result + i;
+		}
+
+		System.out.println("The sun of number from 1 to 10 is "+result);
+	}
+
+}
